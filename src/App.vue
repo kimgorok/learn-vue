4dc20e51285ea9bce9d8bff45b25d3fa18417b02
kimgorok <kjyse2@naver.com>
2025-01-17 19:18:51 +0900

    뷰 영화화

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar />
+  <SearchBar :data="data_temp" @searchMovie="searchMovie($event)" />
+  <p><button @click="showAllMovie">전체 보기</button></p>
+  <Movies
+    :data="data_temp"
+    @increaseLike="increaseLike($event)"
+    @openModal="isModal = true"
+    selectedMovie="$event"
+  />
+
+  <Modal
+    :data="data"
+    :isModal="isModal"
+    :selectedMovie="selectedMovie"
+    @closeModal="isModal = false"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "./assets/movies";
+import Navbar from "./components/Navbar.vue";
+import Modal from "./components/Modal.vue";
+import Movies from "./components/Movies.vue";
+import SearchBar from "./components/SearchBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      isModal: false,
+      selectedMovie: 0,
+      data, // 원본
+      data_temp: [...data], // 사본
+    };
+  },
+  methods: {
+    increaseLike(id) {
+      // this.data[index].like++;
+      this.data.find((movie) => {
+        if (movie.id == id) {
+          movie.like += 1;
+        }
+      });
+    },
+    searchMovie(title) {
+      // 영화 제목이 포함된 데이터를 가져옴
+      this.data_temp = this.data.filter((movie) => {
+        return movie.title.includes(title);
+      });
+    },
+    showAllMovie() {
+      this.data_temp = [...data];
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Navbar: Navbar,
+    Modal: Modal,
+    Movies: Movies,
+    SearchBar: SearchBar,
+  },
+  mounted() {
+    console.log("마운트");
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.bg-yellow {
+  background: yellow;
+}
+
+.modal {
+  background: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal .inner {
+  background: #fff;
+  width: 80%;
+  padding: 20px;
+  border-radius: 10px;
 }
 </style>
---
+++ src/assets/movies.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    id: 1,
+    title: "노량",
+    year: 2023,
+    category: "액션, 드라마",
+    textRed: "color: red",
+    like: 0,
+    img: "./assets/1.png",
+  },
+  {
+    id: 2,
+    title: "아쿠아맨",
+    year: 2023,
+    category: "액션, 판타지",
+    textRed: "color: blue",
+    like: 0,
+    img: "./assets/2.png",
+  },
+  {
+    id: 3,
+    title: "아쿠아맨2",
+    year: 2033,
+    category: "액션, 판타지",
+    textRed: "color: green",
+    like: 0,
+    img: "./assets/3.png",
+  },
+];
+
+export default data;
