bcfc2ab67228d2d2f428aea3f621d515a8f9cc9c
kimgorok <kjyse2@naver.com>
2025-01-19 11:51:13 +0900

    전체 파일에 Prettier 규칙 적용

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    component: () => import("../views/AboutView.vue"),
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
   },
 ];
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     screens: {
-      lg: "1024px",
+      lg: '1024px',
     },
     extend: {
       fontFamily: {
-        pretendard: ["var(--font-pretendard)"],
+        pretendard: ['var(--font-pretendard)'],
       },
       colors: {
         black: {
-          0: "#000000",
+          0: '#000000',
         },
         gray: {
-          0: "#6E6E82",
-          1: "#9FA6B2",
-          2: "#D2D2D2",
-          3: "#A6A6A6",
-          4: "#F3F3F3",
-          5: "#D9D9D9",
+          0: '#6E6E82',
+          1: '#9FA6B2',
+          2: '#D2D2D2',
+          3: '#A6A6A6',
+          4: '#F3F3F3',
+          5: '#D9D9D9',
         },
         blue: {},
         white: {},
