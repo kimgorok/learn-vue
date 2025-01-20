211f9d38a7323829e504da0e8c39c32ab9b1a89b
kimgorok <kjyse2@naver.com>
2025-01-20 14:28:42 +0900

    추가 색상상

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,10 +23,16 @@ const config: Config = {
         gray: {
           0: '#51525B',
           1: '#959393',
+          2: '#D2D2D2',
+          3: '#F5F5F5',
+          4: '#EEEEEE',
         },
         primary: {
           0: '#232D64',
           1: '#223F59',
+          2: '#D7E2FF',
+          3: '#3570FF',
+          4: '#48C5FF',
         },
         white: {
           0: '#FFFFFF',
@@ -36,6 +42,21 @@ const config: Config = {
         blue: {
           0: '#2E2E48',
           1: '#5D5D9D',
+          2: '#7E73FF',
+          3: '#5B4DFF',
+        },
+        red: {
+          0: '#FFCCCC',
+          1: '#FE2222',
+          2: '#FF3C3C',
+        },
+        orange: {
+          0: '#FFE2D1',
+          1: '#FF8738',
+        },
+        green: {
+          0: '#D6F4D0',
+          1: '#45D934',
         },
         // dark: {
         //   bg: '#121212',
